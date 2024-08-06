47217a4011c4de6adb7e9d87949d05067b1b70bf
lianmsu <142714274+lianmsu@users.noreply.github.com>
2024-08-06 16:11:53 +0800

    Update and rename dl_GTDB_classify.sh to GTDB_classify.sh

--- GTDB_classify.sh
+++ GTDB_classify.sh
@@ -5,9 +5,9 @@ binning_path=/lustre/home/liutang_faculty/83LPW/02RDN/12drep/res_merged/dereplic
 start_time=$(date +%s)
  
 mkdir res
-gtdbtk classify_wf --cpus $SLURM_NTASKS --pplacer_cpus  $SLURM_NTASKS  -x fa \
+gtdbtk classify_wf --cpus 28 --pplacer_cpus  28  -x fa \
     --genome_dir ${binning_path}/  \
-    --mash_db /lustre/home/liutang_faculty/02database/gtdbtk_release214/mash  \
+    --mash_db ~/gtdbtk_release214/mash  \
     --out_dir res
 
 end_time=$(date +%s)
